e5b8ba93b6df03437c94d741d3db7b474bb16391
194162565 <wzf194162565@126.com>
2017-11-23 21:18:41 +0800

    codeMaven1

---
+++ maven001/src/main/java/com/bjsxt/test/TestMaven.java
@@ -0,0 +1,20 @@
+package com.bjsxt.test;
+
+import org.junit.Test;
+
+/**
+* @ClassName:TestMaven
+* @Description:
+* @Company: 北京尚学堂科技有限公司 www.bjsxt.com
+* @author mengqx
+* @date 2017年9月28日
+*/
+
+public class TestMaven {
+	
+	@Test
+	public void show(){
+		System.out.println("你好，欢迎来到王者荣耀，敌军还有三秒到达战场！");
+	}
+	
+}
