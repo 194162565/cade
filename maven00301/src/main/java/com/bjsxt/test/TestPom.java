7130d8dae25afcd0915705fec3d95ba7bb0ae571
194162565 <wzf194162565@126.com>
2017-11-24 18:49:04 +0800

    mavenn301

---
+++ maven00301/src/main/java/com/bjsxt/test/TestPom.java
@@ -0,0 +1,19 @@
+package com.bjsxt.test;
+
+import org.junit.Test;
+
+/**
+* @ClassName:TestPom
+* @Description:
+* @Company: 北京尚学堂科技有限公司 www.bjsxt.com
+* @author mengqx
+* @date 2017年9月28日
+*/
+
+public class TestPom {
+	
+	@Test
+	public void show(){
+		System.out.println("大家好，欢迎来看马蓉表演");
+	}
+}
